d36ce447f9db255c029ef985d06b8941d70700c8
amit kumar <amitkumar.cs19@bmsce.ac.in>
2022-11-30 16:47:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+var bodyParser = require('body-parser');
+
+
+app.get("/", function (req, res) {
+    res.render("pages/home");
+  });
+app.get("/:id", function (req, res) {
+    res.render(`pages/${req.params.id}`);
+  });
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
