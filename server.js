41ba75e601d838816c7c1f87a8716e670697fb77
amit kumar <amitkumar.cs19@bmsce.ac.in>
2022-12-16 13:22:46 +0530

    push

--- server.js
+++ server.js
@@ -4,7 +4,24 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 var bodyParser = require('body-parser');
+// mongo db configs using cllg e-id
+const mongoose = require('mongoose')
 
+const url = `mongodb+srv://amit:amit@organsdonor.cd5433k.mongodb.net/?retryWrites=true&w=majority`;
+
+const connectionParams={
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to the database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. n${err}`);
+    })
+
+/////
 
 app.get("/", function (req, res) {
     res.render("pages/index");
